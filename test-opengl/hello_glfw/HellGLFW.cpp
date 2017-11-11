51a166f66c758ee34e6029c2d1b07321a7c9c2bd
lb2016github <HelloLuoBo@126.com>
2017-11-11 17:06:46 +0800

    add files

---
+++ test-opengl/hello_glfw/HellGLFW.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "HellGLFW.h"
+
+
+HellGLFW::HellGLFW()
+{
+}
+
+
+HellGLFW::~HellGLFW()
+{
+}
---
+++ test-opengl/hello_glfw/HellGLFW.h
@@ -0,0 +1,16 @@
+#pragma once
+class HellGLFW
+{
+public:
+	HellGLFW();
+	~HellGLFW();
+
+/*
+vitural functions
+*/
+public:
+	
+private:
+
+};
+
