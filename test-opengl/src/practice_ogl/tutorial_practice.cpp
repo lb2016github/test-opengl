60e3856c256cfdc89114d8fa9bf0bf801abb0419
luobo <HelloLuoBo@126.com>
2018-04-14 17:14:08 +0800

    modify project config

--- test-opengl/src/practice_ogl/tutorial_practice.cpp
+++ test-opengl/src/practice_ogl/tutorial_practice.cpp
@@ -1,8 +1,9 @@
 #include "util\practice_header.h"
+#include "util\util.h"
 #include <glad\glad.h>
 
 
-GLuint vbo;
+GLuint vbo, program_id;
 
 static const GLfloat vertex_data[] = {
 	-1.0f, -1.0f, 0.0f,
@@ -10,18 +11,19 @@ static const GLfloat vertex_data[] = {
 	0.0f, 1.0f, 0.0f,
 };
 
-
-
 // 初始化
 void gl_init() {
-	/*glGenBuffers(1, &vbo);
+	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), vertex_data, GL_STATIC_DRAW);*/
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), vertex_data, GL_STATIC_DRAW);
+
+	program_id = LoadShaders("tutorial_2.vs", "tutorial_2.ps");
+	glUseProgram(program_id);
 }
 
 // 更新
 void gl_update(float width, float height, float delta_time) {
-	/*glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(
 		0,
 		3,
@@ -31,11 +33,11 @@ void gl_update(float width, float height, float delta_time) {
 		(void*)0
 	);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glDisableVertexAttribArray(0);*/
+	glDisableVertexAttribArray(0);
 
 }
 
 // 销毁
 void gl_destroy() {
-	//glDeleteBuffers(1, &vbo);
+	glDeleteBuffers(1, &vbo);
 }
