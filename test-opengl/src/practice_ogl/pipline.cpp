411b329a8a800b83e85ab90df3af421a1bf3c55b
luobo <HelloLuoBo@126.com>
2018-05-17 18:05:13 +0800

    1)add pipline class; 2)add ambiance shader

---
+++ test-opengl/src/practice_ogl/pipline.cpp
@@ -0,0 +1,105 @@
+#include "pipline.h"
+
+CameraInfo::CameraInfo(const CameraInfo& cam_info) {
+	m3dCopyVector3(pos, cam_info.pos);
+	m3dCopyVector3(target, cam_info.target);
+	m3dCopyVector3(up, cam_info.up);
+}
+
+CameraInfo& CameraInfo::operator=(const CameraInfo& cam_info) {
+	m3dCopyVector3(pos, cam_info.pos);
+	m3dCopyVector3(target, cam_info.target);
+	m3dCopyVector3(up, cam_info.up);
+	return *this;
+}
+
+Pipline::Pipline()
+{
+	m_world_pos[0] = 0;
+	m_world_pos[1] = 0;
+	m_world_pos[2] = 0;
+
+	m_scale[0] = 1;
+	m_scale[1] = 1;
+	m_scale[2] = 1;
+
+	m_rotation[0] = 0;
+	m_rotation[1] = 0;
+	m_rotation[2] = 0;
+}
+
+Pipline::~Pipline()
+{
+}
+
+void Pipline::set_world_pos(float x, float y, float z) {
+	m_world_pos[0] = x;
+	m_world_pos[1] = y;
+	m_world_pos[2] = z;
+}
+
+void Pipline::set_scale(float scale) {
+	m_scale[0] = scale;
+	m_scale[1] = scale;
+	m_scale[2] = scale;
+}
+
+void Pipline::set_rotation(float x, float y, float z) {
+	m_rotation[0] = x;
+	m_rotation[1] = y;
+	m_rotation[2] = z;
+}
+
+void Pipline::set_pers_proj_info(float fov, float width, float height, float z_near, float z_far) {
+	m_pp_info.fov = fov;
+	m_pp_info.width = width;
+	m_pp_info.height = height;
+	m_pp_info.z_near = z_near;
+	m_pp_info.z_far = z_far;
+}
+
+void Pipline::set_orthor_proj_info(float right, float left, float back, float top, float z_near, float z_far) {
+	m_op_info.right = right;
+	m_op_info.left = left;
+	m_op_info.back = back;
+	m_op_info.top = top;
+	m_op_info.z_near = z_near;
+	m_op_info.z_far = z_far;
+}
+
+void Pipline::set_camera_info(M3DVector3f pos, M3DVector3f target, M3DVector3f up) {
+	m3dCopyVector3(m_cam_info.pos, pos);
+	m3dCopyVector3(m_cam_info.target, target);
+	m3dCopyVector3(m_cam_info.up, up);
+}
+
+void Pipline::get_world_trans(M3DMatrix44f w) {
+	M3DMatrix44f tmp_pos, tmp_scale, tmp_rotation, tmp_ps;
+	m3dTranslationMatrix44(tmp_pos, m_world_pos[0], m_world_pos[1], m_world_pos[2]);
+	m3dScaleMatrix44(tmp_scale, m_scale[0], m_scale[1], m_scale[2]);
+	m3dRotationMatrix44(tmp_rotation, m_rotation[0], m_rotation[1], m_rotation[2]);
+
+	m3dMatrixMultiply44(tmp_ps, tmp_pos, tmp_scale);
+	m3dMatrixMultiply44(w, tmp_ps, tmp_rotation);
+}
+
+void Pipline::get_view_trans(M3DMatrix44f v) {
+	M3DMatrix44f pos, rot;
+	m3dTranslationMatrix44(pos, m_cam_info.pos[0], m_cam_info.pos[1], m_cam_info.pos[2]);
+	m3dCameraMatrix44(rot, m_cam_info.target, m_cam_info.up);
+	m3dMatrixMultiply44(v, pos, rot);
+}
+
+void Pipline::get_pers_proj_trans(M3DMatrix44f p) {
+	float aspect = m_pp_info.width / m_pp_info.height;
+	m3dMakePerspectiveMatrix(p, m_pp_info.fov, aspect, m_pp_info.z_near, m_pp_info.z_far);
+}
+
+void Pipline::get_wvp_trans(M3DMatrix44f wvp) {
+	M3DMatrix44f w, v, p, tmp;
+	get_world_trans(w);
+	get_view_trans(v);
+	get_pers_proj_trans(p);
+	m3dMatrixMultiply44(tmp, w, v);
+	m3dMatrixMultiply44(wvp, tmp, p);
+}
---
+++ test-opengl/src/practice_ogl/pipline.h
@@ -0,0 +1,72 @@
+#ifndef _PIPLINE_H
+#define _PIPLINE_H
+
+#include "math3d\math3d.h"
+
+class PersProjInfo {
+public:
+	float fov;
+	float width;
+	float height;
+	float z_near;
+	float z_far;
+};
+
+class OrthorProjInfo {
+public:
+	float right;
+	float left;
+	float back;
+	float top;
+	float z_near;
+	float z_far;
+};
+
+class CameraInfo {
+public:
+	CameraInfo() {}
+	CameraInfo(const CameraInfo& cam_info);
+	CameraInfo& operator=(const CameraInfo& cam_info);
+
+	M3DVector3f pos;
+	M3DVector3f target;
+	M3DVector3f up;
+};
+
+class Pipline
+{
+public:
+	Pipline();
+	~Pipline();
+
+	void set_world_pos(M3DVector3f world_pos) { m3dCopyVector3(m_world_pos, world_pos); }
+	void set_world_pos(float x, float y, float z);
+	void set_scale(M3DVector3f scale) { m3dCopyVector3(m_scale, scale); }
+	void set_scale(float scale);
+	void set_rotation(M3DVector3f rotation) { m3dCopyVector3(m_rotation, rotation); }
+	void set_rotation(float x, float y, float z);
+
+	void set_pers_proj_info(PersProjInfo& pp_info) { m_pp_info = pp_info; }
+	void set_pers_proj_info(float fov, float width, float height, float z_near, float z_far);
+	void set_orthor_proj_info(OrthorProjInfo& op_info) { m_op_info = op_info; }
+	void set_orthor_proj_info(float right, float left, float back, float top, float z_near, float z_far);
+	void set_camera_info(CameraInfo& cam_info) { m_cam_info = cam_info; }
+	void set_camera_info(M3DVector3f pos, M3DVector3f target, M3DVector3f up);
+
+	void get_world_trans(M3DMatrix44f w);
+	void get_view_trans(M3DMatrix44f v);
+	void get_pers_proj_trans(M3DMatrix44f p);
+	void get_wvp_trans(M3DMatrix44f wvp);
+
+private:
+	M3DVector3f m_world_pos;
+	M3DVector3f m_scale;
+	M3DVector3f m_rotation;
+
+	PersProjInfo m_pp_info;
+	OrthorProjInfo m_op_info;
+	
+	CameraInfo m_cam_info;
+};
+
+#endif // !_PIPLINE_H
